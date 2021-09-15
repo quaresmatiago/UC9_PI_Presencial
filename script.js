878cc38d9594a91ace67f476d69569e4b24c16ed
tiago <quaresmatiago@terra.com.br>
2021-09-14 22:31:26 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,7 @@
+//Mascara
+$(function() {
+    $(".cpf_mask").mask('999.999.999-99');
+    $(".tel_res_mask").mask('(99)9999-9999');
+    $(".tel_cel_mask").mask('(99)99999-9999');
+    $(".cep_mask").mask('99999-999');
+});
